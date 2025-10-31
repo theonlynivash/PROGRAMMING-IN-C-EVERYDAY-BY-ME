924a655d4d187e4ce17a65a10433c0274dc31376
VLN_NIVASH <theonlynivash@gmail.com>
2025-10-31 08:12:49 +0530

    Add files via upload

---
+++ P12.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, first = 0, second = 1, next, i;
+
+    // Ask user for number of terms
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= n; i++) {
+        printf("%d ", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    printf("\n");
+    return 0;
+}
